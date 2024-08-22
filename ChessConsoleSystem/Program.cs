64a7324394849c00cb5dc075cf2230a0ca05f9f8
Brendon Gomes <brendongomes40@gmail.com>
2024-08-21 21:56:39 -0300

    Adicionar arquivos de projeto.

---
+++ ChessConsoleSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessConsoleSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
